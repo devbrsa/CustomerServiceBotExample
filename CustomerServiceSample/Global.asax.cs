f2dc991b2d48ba4447e1fff883e2d58ea9c4399f
Bruno Arruda <arruda@outlook.com>
2018-07-30 22:01:19 +0200

    Add project files.

---
+++ CustomerServiceSample/Global.asax.cs
@@ -0,0 +1,47 @@
+﻿using Autofac;
+using Microsoft.Bot.Builder.Azure;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Internals;
+using Microsoft.Bot.Connector;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace CustomerServiceSample
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            Conversation.UpdateContainer(
+            builder =>
+            {
+                builder.RegisterModule(new AzureModule(Assembly.GetExecutingAssembly()));
+
+                // Bot Storage: Here we register the state storage for your bot. 
+                // Default store: volatile in-memory store - Only for prototyping!
+                // We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
+                // For samples and documentation, see: [https://github.com/Microsoft/BotBuilder-Azure](https://github.com/Microsoft/BotBuilder-Azure)
+
+                //var store = new InMemoryDataStore();
+                var store = new TableBotDataStore(ConfigurationManager.ConnectionStrings["StorageConnectionString"].ConnectionString);
+
+                // Other storage options
+                // var store = new TableBotDataStore("...DataStorageConnectionString..."); // requires Microsoft.BotBuilder.Azure Nuget package 
+                // var store = new DocumentDbBotDataStore("cosmos db uri", "cosmos db key"); // requires Microsoft.BotBuilder.Azure Nuget package 
+
+                builder.Register(c => store)
+                    .Keyed<IBotDataStore<BotData>>(AzureModule.Key_DataStore)
+                    .AsSelf()
+                    .SingleInstance();
+            });
+        }
+    }
+}
